9a39dbdb0780e653cb8ad8aa52ee16e62d4075b2
bruno.ribeiro <bruno.ribeiro@dynamicyield.com>
2024-12-02 23:35:11 +0000

    day 2

--- src/main/kotlin/day02/Day02.kt
+++ src/main/kotlin/day02/Day02.kt
@@ -15,16 +15,12 @@ fun main() {
         .map { it.split("\\s+".toRegex()).map { it.toInt() } }
         .filter { validateReport(it.zipWithNext()) }.size
 
-
     fun part2(input: List<String>) = input
         .map { it.split("\\s+".toRegex()).map { it.toInt() } }
         .filterNot { validateReport(it.zipWithNext()) }
         .filter { report ->
-            (report.indices.map { index ->
-                report.filterIndexed { i, _ -> i != index }
-            }).any { validateReport(it.zipWithNext()) }
-        }
-        .size
+            report.indices.any { index -> validateReport(report.filterIndexed { i, _ -> i != index }.zipWithNext()) }
+        }.size
 
 
     part1(readInput("day02/input")).println()
